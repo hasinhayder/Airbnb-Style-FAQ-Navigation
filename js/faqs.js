3b2d127fb10a3eada74301d4331750b372d3ee81
Hasin Hayder <ironhide@leevio.com>
2015-09-12 03:45:08 +0600

    Airbnb Style FAQ Nav

---
+++ js/faqs.js
@@ -0,0 +1,26 @@
+var faqs = [
+  {
+    title: "Let's Talk About Moon",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. <img src='https://d13yacurqjgara.cloudfront.net/users/31752/screenshots/2236968/anduin_1x.png'/> Blanditiis cum delectus excepturi ipsum maxime quis quod veritatis voluptatum! Autem blanditiis deserunt dolorum esse excepturi laudantium minima omnis quia reiciendis repudiandae?"
+  },
+  {
+    title: "Hello World",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis cum delectus excepturi ipsum maxime quis quod veritatis voluptatum! Autem blanditiis deserunt dolorum esse excepturi laudantium minima omnis quia reiciendis repudiandae?"
+  },
+  {
+    title: "Dark Side Of The Moon",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. <img src='https://d13yacurqjgara.cloudfront.net/users/11867/screenshots/2237038/zombie_01_1x.jpg'/> Blanditiis cum delectus excepturi ipsum maxime quis quod veritatis voluptatum! Autem blanditiis deserunt dolorum esse excepturi laudantium minima omnis quia reiciendis repudiandae?"
+  },
+  {
+    title: "UFO Found on Pluto",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis cum delectus excepturi ipsum maxime quis quod veritatis voluptatum! Autem blanditiis deserunt dolorum esse excepturi laudantium minima omnis quia reiciendis repudiandae?"
+  },
+  {
+    title: "We're Not Alone",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis cum delectus excepturi ipsum maxime quis quod veritatis voluptatum! Autem blanditiis deserunt dolorum esse excepturi laudantium minima omnis quia reiciendis repudiandae?"
+  },
+  {
+    title: "Breaking: Water on Mars!",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis cum delectus excepturi ipsum maxime quis quod veritatis voluptatum! Autem blanditiis deserunt dolorum esse excepturi laudantium minima omnis quia reiciendis repudiandae?"
+  }
+];
---
+++ js/react-faq.js
@@ -0,0 +1,103 @@
+/**
+ * @author: Hasin Hayder
+ */
+
+var GoBack = React.createClass({
+  render: function () {
+    var display = !this.props.dp ? 'dn' : '';
+    return (
+      <div className={'faq-back ' + display}>
+        <a href="#" onClick={this.props.onClick}>&lt;&lt; All Articles</a>
+      </div>
+    );
+  }
+});
+
+var FAQ = React.createClass({
+  getInitialState: function () {
+    return {
+      item: this.props.item
+    };
+  },
+
+  render: function () {
+    var display = this.state.item.visible ? '' : 'dn';
+    var detailDisplay = this.state.item.detail ? '' : 'dn';
+    return (
+      <div className={'faq-item ' + display }>
+        <div className='faq-icon'>
+          <img src='img/faq.png'></img>
+        </div>
+        <h2 className='faq-title' onClick={this.props.onClick}>{this.state.item.title}</h2>
+        <div className={'faq-content ' + detailDisplay}>
+          <p dangerouslySetInnerHTML={{__html:this.state.item.text}}></p>
+        </div>
+        <div className="clear"></div>
+      </div>
+    );
+  }
+});
+
+var FAQs = React.createClass({
+  getInitialState: function () {
+    var faqs = this.props.faqs.map(function (faq, index) {
+      faq.visible = 1;
+      faq.detail = 0;
+      faq.index = index;
+      return faq;
+    });
+
+    return {
+      faqs: faqs,
+      showGoBack: 0
+    };
+  },
+
+  goBack: function (e) {
+    e.preventDefault();
+    var items = this.state.faqs.map(function (item) {
+      item.visible = 1;
+      item.detail = 0;
+      return item;
+    });
+    this.setState({faqs: items, showGoBack: 0});
+  },
+
+  faqClicked: function (faq) {
+    var items = this.state.faqs.map(function (item) {
+      item.visible = 0;
+      item.detail = 0;
+      return item;
+    });
+    items[faq.index].visible = 1;
+    items[faq.index].detail = 1;
+    this.setState({faqs: items, showGoBack: 1});
+  },
+
+  render: function () {
+    var that = this;
+    return (
+      <div onMouseOver={this.toggleFAQ} onMouseOut={this.toggleFAQ}>
+        <span className='faq-nav'> FAQ&nbsp; </span>
+        <div ref='fc' className='faq-container dn'>
+          <GoBack onClick={this.goBack} dp={this.state.showGoBack}/>
+          <div className='faqs'>
+            {
+              this.state.faqs.map(function (item, index) {
+                if ( item.visible ) {
+                  var faqClicked = that.faqClicked.bind(null, item);
+                  return <FAQ onClick={faqClicked} key={index} item={item}/>
+                }
+              })
+            }
+          </div>
+        </div>
+      </div>
+    );
+  },
+  toggleFAQ: function () {
+    React.findDOMNode(this.refs['fc']).classList.toggle('dn');;
+  }
+
+});
+React.render(<FAQs faqs={faqs}/>, document.getElementById('react-faq'));
